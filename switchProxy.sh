dc66d630eac3da3e2b381200cd86c71f0e48aab7
Daniel Rodríguez Rivero <rdanielo@gmail.com>
2015-09-02 11:20:45 +0200

    Delete switchProxy.sh

--- switchProxy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-hasproxy=$(git config --local --get http.proxy)
-proxy="proxy-fr-croissy.gemalto.com:8080"
-if [ -z "$proxy" ]; then
- git config --local http.proxy $proxy
-else
- git config --local http.proxy ""
-fi
